0305664d7d842c350205acb547a9664bd915a35f
Gustavo Santos <devgustavosantos@gmail.com>
2022-10-19 14:46:08 -0300

    feat: add theme colors to project

---
+++ src/styles/theme.js
@@ -0,0 +1,21 @@
+export default {
+  COLORS: {
+    BLUE_900: "#01070a",
+    BLUE_800: "#001119",
+    BLUE_700: "#01131d",
+    BLUE_600: "#0d1721",
+    BLUE_500: "#0d1d25",
+    BLUE_400: "#141d22",
+    BLUE_300: "#82f3ff",
+
+    GRAY_900: "#7c7c8a",
+    GRAY_800: "#666c6f",
+
+    RED_900: "#92000e",
+    RED_800: "#ab4d55",
+
+    ORANGE: "#fba94c",
+    GREEN: "#04d361",
+    WHITE: "#ffffff",
+  },
+};
