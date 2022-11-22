73e84f212b0b816e701b65db1fae5a0fe5b10d48
Gustavo Santos <devgustavosantos@gmail.com>
2022-11-22 15:33:53 -0300

    feat: adds functionality to load orders

--- src/hooks/request.jsx
+++ src/hooks/request.jsx
@@ -11,9 +11,7 @@ function RequestProvider({ children }) {
           try {
             const response = await api.get(resource);
 
-            const { data } = response;
-
-            resolve(data);
+            resolve(response);
           } catch (error) {
             reject(error);
           }
--- src/pages/AllOrders/index.jsx
+++ src/pages/AllOrders/index.jsx
@@ -1,5 +1,5 @@
-import { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Fragment, useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import { Container, Content, Table } from "./styles";
 
@@ -7,78 +7,19 @@ import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Wrapper } from "../../components/Wrapper";
 import { Select } from "../../components/Select";
+import { Loading } from "../../components/Loading";
+import { useRequest } from "../../hooks/request";
 
 export function AllOrders() {
-  const [orders, setOrders] = useState([
-    {
-      id: 32,
-      status: "delivered",
-      created_at: "2022-10-18 14:27:32",
-      meals: [
-        {
-          order_meal_id: 44,
-          title: "Strogonoff",
-          meal_amount: 2,
-        },
-        {
-          order_meal_id: 43,
-          title: "Macarrão 4",
-          meal_amount: 5,
-        },
-        {
-          order_meal_id: 42,
-          title: "Pizza",
-          meal_amount: 3,
-        },
-      ],
-    },
-    {
-      id: 31,
-      status: "pending",
-      created_at: "2022-10-17 18:20:35",
-      meals: [
-        {
-          order_meal_id: 41,
-          title: "Strogonoff",
-          meal_amount: 2,
-        },
-        {
-          order_meal_id: 40,
-          title: "Macarrão",
-          meal_amount: 1,
-        },
-        {
-          order_meal_id: 39,
-          title: "Pizza",
-          meal_amount: 3,
-        },
-      ],
-    },
-    {
-      id: 30,
-      status: "preparing",
-      created_at: "2022-10-17 15:00:08",
-      meals: [
-        {
-          order_meal_id: 38,
-          title: "Macarrão 6",
-          meal_amount: 5,
-        },
-        {
-          order_meal_id: 37,
-          title: "Macarrão",
-          meal_amount: 3,
-        },
-        {
-          order_meal_id: 36,
-          title: "Pizza",
-          meal_amount: 2,
-        },
-      ],
-    },
-  ]);
+  const [orders, setOrders] = useState();
+
+  console.log({ orders });
 
   const [isAdm, setIsAdm] = useState(false);
+
+  const { manageRequests } = useRequest();
+
+  const navigate = useNavigate();
 
   function formatDateTime(datetime) {
     const [date, time] = datetime.split(" ");
@@ -91,62 +32,96 @@ export function AllOrders() {
 
     return formattedDateAndTime;
   }
+
+  useEffect(() => {
+    async function fetchOrders() {
+      const response = await manageRequests("get", "/orders");
+
+      console.log({ response });
+
+      const theRequestWasSuccessful = Array.isArray(response.data);
+
+      console.log({ theRequestWasSuccessful });
+
+      if (theRequestWasSuccessful) {
+        setOrders(response.data);
+
+        return;
+      }
+
+      if (response.data) {
+        alert(response.data.message);
+      } else {
+        alert(
+          "Não foi possível carregar os pedidos! Por favor tente novamente mais tarde."
+        );
+      }
+      return navigate("/");
+    }
+
+    fetchOrders();
+  }, []);
+
   return (
     <Container>
       <Header />
       <Wrapper>
-        <Content>
-          <h1>Pedidos</h1>
-          <Table>
-            <table>
-              <thead>
-                <tr>
-                  <th>Status</th>
-                  <th>Código</th>
-                  <th>Detalhamento</th>
-                  <th>Data e hora</th>
-                </tr>
-              </thead>
-              <tbody>
-                {orders.map(order => {
-                  const { id, status, meals, created_at } = order;
+        {!orders ? (
+          <Loading />
+        ) : (
+          <Content>
+            <h1>Pedidos</h1>
+            <Table>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Status</th>
+                    <th>Código</th>
+                    <th>Detalhamento</th>
+                    <th>Data e hora</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orders.map(order => {
+                    const { id, status, meals, created_at } = order;
 
-                  return (
-                    <tr key={String(id)}>
-                      <td>
-                        {
-                          <Select
-                            order_id={id}
-                            status={status}
-                            disabled={!isAdm}
-                          />
-                        }
-                      </td>
-                      <td>
-                        <Link to={`/order/${id}`}>
-                          {String(id).padStart(8, "0")}
-                        </Link>
-                      </td>
-                      <td>
-                        {meals.map((meal, index) => {
-                          const lastItemInArray = index == meals.length - 1;
+                    return (
+                      <tr key={String(id)}>
+                        <td>
+                          {
+                            <Select
+                              order_id={id}
+                              status={status}
+                              disabled={!isAdm}
+                            />
+                          }
+                        </td>
+                        <td>
+                          <Link to={`/order/${id}`}>
+                            {String(id).padStart(8, "0")}
+                          </Link>
+                        </td>
+                        <td>
+                          {meals.map((meal, index) => {
+                            const lastItemInArray = index == meals.length - 1;
 
-                          return (
-                            <Fragment key={meal.order_meal_id}>
-                              {meal.meal_amount}x {meal.title}
-                              {lastItemInArray ? "" : ", "}
-                            </Fragment>
-                          );
-                        })}
-                      </td>
-                      <td>{formatDateTime(created_at)}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </Table>
-        </Content>
+                            return (
+                              <Fragment key={meal.order_meal_id}>
+                                {meal.meal_amount}x {meal.title}
+                                {lastItemInArray ? "" : ", "}
+                              </Fragment>
+                            );
+                          })}
+                        </td>
+                        <td>{formatDateTime(created_at)}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </Table>
+          </Content>
+        )}
       </Wrapper>
       <Footer />
     </Container>
