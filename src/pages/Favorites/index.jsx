17e5140a13b035a2eb36e83bf019ab9618a6a17e
Gustavo Santos <devgustavosantos@gmail.com>
2022-11-23 08:50:51 -0300

    feat: adds loading screen in 'Favorites' interface

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -6,10 +6,13 @@ import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Wrapper } from "../../components/Wrapper";
 import { Card } from "../../components/Card";
+import { Loading } from "../../components/Loading";
 import { useRequest } from "../../hooks/request";
 
 export function Favorites() {
   const [favorites, setFavorites] = useState();
+
+  console.log({ favorites });
 
   const { manageRequests } = useRequest();
 
@@ -45,33 +48,37 @@ export function Favorites() {
     <Container>
       <Header />
       <Wrapper>
-        <Content>
-          {!favorites || favorites.length == 0 ? (
-            <h1>Você ainda não tem favoritos</h1>
-          ) : (
-            <>
-              <h1>Favoritos</h1>
-              <section className="cards">
-                {favorites &&
-                  favorites.map(favorite => {
-                    const { title, description, id, image, price } = favorite;
+        {!favorites ? (
+          <Loading />
+        ) : (
+          <Content>
+            {favorites.length == 0 ? (
+              <h1>Você ainda não tem favoritos</h1>
+            ) : (
+              <>
+                <h1>Favoritos</h1>
+                <section className="cards">
+                  {favorites &&
+                    favorites.map(favorite => {
+                      const { title, description, id, image, price } = favorite;
 
-                    return (
-                      <Card
-                        key={String(id)}
-                        image={image}
-                        title={title}
-                        description={description}
-                        meal_id={id}
-                        price={price}
-                        isFav={true}
-                      />
-                    );
-                  })}
-              </section>
-            </>
-          )}
-        </Content>
+                      return (
+                        <Card
+                          key={String(id)}
+                          image={image}
+                          title={title}
+                          description={description}
+                          meal_id={id}
+                          price={price}
+                          isFav={true}
+                        />
+                      );
+                    })}
+                </section>
+              </>
+            )}
+          </Content>
+        )}
       </Wrapper>
       <Footer />
     </Container>
