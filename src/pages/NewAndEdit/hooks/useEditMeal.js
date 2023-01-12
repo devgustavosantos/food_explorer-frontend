75cec4c2f9d2b50297e3ed24040d9d1a9237a1e5
Gustavo Santos <devgustavosantos@gmail.com>
2023-01-12 08:24:33 -0300

    refactor: improves the logic of the functionality of editing a meal

--- src/pages/NewAndEdit/hooks/useEditMeal.js
+++ src/pages/NewAndEdit/hooks/useEditMeal.js
@@ -18,48 +18,76 @@ export function useEditMeal({
 
   const { manageRequests } = useRequest();
 
-  async function handleEditMeal() {
-    inputsValidation();
-
-    const priceFormatted = formatPrice(price);
-
-    console.log({ priceFormatted });
-
-    const mealResponse = await manageRequests('put', `/meals/${mealInfos.id}`, {
+  async function requestUpdateMeal() {
+    const response = await manageRequests('put', `/meals/${mealInfos.id}`, {
       title,
       category,
-      price: priceFormatted,
+      price: formatPrice(price),
       description,
       ingredients,
     });
+
+    return response;
+  }
+
+  async function handleMealUpdate() {
+    const mealResponse = await requestUpdateMeal();
 
     validateRequest({
       response: mealResponse,
       devMessage: 'The meal update was not successful.',
     });
+  }
 
-    const isTheSameImage = mealInfos.image === photo;
+  async function requestUpdatePhoto(fileForm) {
+    const response = await manageRequests(
+      'patch',
+      `/meals/${mealInfos.id}`,
+      fileForm
+    );
 
-    if (!isTheSameImage) {
-      const fileForm = new FormData();
+    return response;
+  }
 
-      fileForm.append('image', photo);
+  function createFileForm() {
+    const fileForm = new FormData();
 
-      const imageResponse = await manageRequests(
-        'patch',
-        `/meals/${mealInfos.id}`,
-        fileForm
-      );
+    fileForm.append('image', photo);
 
-      validateRequest({
-        response: imageResponse,
-        devMessage: 'The image meal update was not successful.',
-      });
-    }
+    return fileForm;
+  }
 
+  async function handleImageUpdate() {
+    const fileForm = createFileForm();
+
+    const imageResponse = await requestUpdatePhoto(fileForm);
+
+    validateRequest({
+      response: imageResponse,
+      devMessage: 'The image meal update was not successful.',
+    });
+  }
+
+  async function validateImageUpdate() {
+    const theImageHasBeenChanged = mealInfos.image !== photo;
+
+    if (theImageHasBeenChanged) await handleImageUpdate();
+  }
+
+  function handleEditSuccess() {
     alert('O prato foi atualizado com sucesso!');
 
     navigate(`/details/${mealInfos.id}`);
+  }
+
+  async function handleEditMeal() {
+    inputsValidation();
+
+    await handleMealUpdate();
+
+    await validateImageUpdate();
+
+    handleEditSuccess();
   }
 
   return {
